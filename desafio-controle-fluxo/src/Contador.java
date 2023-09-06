15f69b30baa64b9da73a863d454aef554758d44e
Bruno Henrique Marinho Marques <brunohenriw1@gmail.com>
2023-09-06 14:23:50 -0300

    Classe para retornar Exception

--- desafio-controle-fluxo/src/Contador.java
+++ desafio-controle-fluxo/src/Contador.java
@@ -2,11 +2,45 @@ import java.util.*;
 public class Contador {
     public static void main(String[] args) {
     
-        Scanner scan = new Scanner(System.in);
+       //criando o método scanner
+       Scanner scan = new Scanner(System.in);
 
-        
+       System.out.println("Digite o primeiro número: ");
+       int numeroUm = scan.nextInt();
 
+       System.out.println("Digite o segundo número");
+       int numeroDois = scan.nextInt();
+
+       //tenta executar o que está dentro de try
+       try{
+            contar(numeroUm, numeroDois);
+       }
+       //senão retorna esta exceção caso try não execute
+       catch(ParametrosInvalidosException exception){
+        System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+       }
+       
 
     }
     
+    //criado o método para contar
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+
+        if(parametroUm>parametroDois){
+            throw new ParametrosInvalidosException();
+        }
+        else{
+
+
+            int contagem  = parametroDois - parametroUm;
+            for(int x=1; x<=contagem;x++){
+            System.out.println(x);
+            }
+
+        }
+
+        
+        
+        
+    }
 }
